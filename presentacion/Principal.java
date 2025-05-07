93238d9e476ab7ffb209cfeecd2036ac39fac191
juuliiveeraa <j.veraq@uniandes.edu.co>
2025-05-07 09:23:57 -0500

    view

--- presentacion/Principal.java
+++ presentacion/Principal.java
@@ -1,37 +1,64 @@
 package presentacion;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import logica.Parcial;
 
-public class Principal {
+public class Principal extends JFrame implements ActionListener{
+	private JLabel resultado;
+	private JButton botonCalcular;
+	private JPanel panel;
 	private Parcial parcial;
 	private int b;
-	private int e;
-	
-	public Principal() {
-		this.parcial = new Parcial(b, e);
-		
-		try {
-			int b = 2;
-			int e = 3;
-			
-			this.parcial.setB(b);
-			this.parcial.setE(e);
-			
-			this.parcial.potencia(b, e);
+	private int exp;
 
-			System.out.println("Base: " + this.parcial.getB());
-			System.out.println("Exponente: " + this.parcial.getE());
-			
-			int resultado = this.parcial.potencia(b, e);
-			System.out.println("Resultado: " + resultado);
-		} 
-		catch (ParcialException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
+	public Principal() {
+		this.parcial = new Parcial(b, exp);
+		
+		this.setTitle("Parcial 2: Calcular Potencias");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setSize(700, 700);
+		this.setLayout(new BorderLayout());	
+		
+		this.resultado = new JLabel("0", SwingConstants.RIGHT);
+		this.resultado.setBackground(Color.white);
+		this.resultado.setOpaque(true);
+		
+		this.add(this.resultado, BorderLayout.SOUTH);
+		
+		this.panel = new JPanel();
+		this.add(this.panel);
+		
+		this.botonCalcular = new JButton();
+
+		this.botonCalcular.setText("Calcular potencia");
+
+		this.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
 		new Principal();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		JButton botonPresionado = (JButton)e.getSource();
+		int resultado = this.parcial.potencia(b, exp);
+		String resultadoString = resultado
+		this.resultado.setText(resultadoString);
 	}
 }
